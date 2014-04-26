b484af5cefff3402b6296464eb41ed23ceda8b32
laferrera <jason.laferrera@gmail.com>
2014-04-26 13:24:02 -0400

    heroku configs

--- web.rb
+++ web.rb
@@ -7,8 +7,10 @@ include Mongo
 configure do
   if ENV['MONGOHQ_URL']
      conn = Mongo::Connection.from_uri(ENV['MONGOHQ_URL'])
-     uri = URI.parse(ENV['MONGOHQ_URL'])
-     config.master = conn.db(uri.path.gsub(/^\//, ''))
+     # uri = URI.parse(ENV['MONGOHQ_URL'])
+     # config.master = conn.db(uri.path.gsub(/^\//, ''))
+     set :mongo_connection, conn
+     set :mongo_db, conn.db('test')     
  else
     conn = MongoClient.new("localhost", 27017)
     set :mongo_connection, conn
